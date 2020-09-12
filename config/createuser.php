bbf2a8d58dc3e628e436b69fbd8966ba4229b0ee
Andreas Wiedel <dmp@nfze.de>
2020-09-12 14:53:48 +0200

    Changed default model namespace to fit the default structure of Laravel 8

--- config/createuser.php
+++ config/createuser.php
@@ -6,7 +6,7 @@ return [
     /*
     * The class name of the user model to be used.
     */
-    'model' => 'App\User',
+    'model' => 'App\Models\User',
 
     /*
     * The fields with their validation rules to check for user model input.
